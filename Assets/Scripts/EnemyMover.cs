4e691b697a06af20618127fd9dc98ae8836d5438
Ewan Guscott <ewanguscott@hotmail.co.uk>
2023-12-12 20:34:23 +0000

    Added manually decided enemy movement

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path =  new List<Waypoint>();
+    [SerializeField] float waitTime = 1f;
 
     private void Start() {
-        PrintWaypointName();
+        StartCoroutine(FollowPath());
     }
-    void PrintWaypointName()
+    IEnumerator FollowPath()
     {
         foreach(Waypoint waypoint in path)
         {
-            Debug.Log(waypoint.name);
+            this.transform.position = new Vector3(waypoint.transform.position.x,transform.position.y, waypoint.transform.position.z);
+            yield return new WaitForSeconds(waitTime);
         }
     }
 }
