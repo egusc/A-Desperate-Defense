bcfccf036d8d26e47f71c101d64c14b7bcbca898
Ewan Guscott <ewanguscott@hotmail.co.uk>
2023-12-12 21:31:40 +0000

    Added smooth enemy movement and mouse over for tiles

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path =  new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range(0f,5f)] float moveSpeed = 1f;
 
     private void Start() {
         StartCoroutine(FollowPath());
@@ -14,8 +14,17 @@ public class EnemyMover : MonoBehaviour
     {
         foreach(Waypoint waypoint in path)
         {
-            this.transform.position = new Vector3(waypoint.transform.position.x,transform.position.y, waypoint.transform.position.z);
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = new Vector3(waypoint.transform.position.x,transform.position.y, waypoint.transform.position.z);
+            float travelPercent = 0f;       //Percentage of distance the enemy has travelled at a certain point
+            transform.LookAt(endPosition);
+            while(travelPercent < 1f)
+            {
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                travelPercent += Time.deltaTime * moveSpeed;
+                yield return new WaitForEndOfFrame();
+            }
+            
         }
     }
 }
