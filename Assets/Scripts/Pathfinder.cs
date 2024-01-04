7671c34cb70de99e1ba43ce15c52fb80112dea6f
Ewan Guscott <ewanguscott@hotmail.co.uk>
2024-01-04 21:16:11 +0000

    Finds Neighbour Node of first coordinate

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour
 {
+    [SerializeField] Node currentSearchNode;
+    Vector2Int[] directions = { Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down };
+    GridManager gridManager;
+    Dictionary<Vector2Int, Node> grid;
+
+    private void Awake() {
+        gridManager = FindObjectOfType<GridManager>();
+        grid = gridManager.Grid;
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        ExploreNeighbours();
     }
 
-    // Update is called once per frame
-    void Update()
+    void ExploreNeighbours()
     {
+        List<Node> neighbours = new List<Node>();
         
+        foreach(Vector2Int direction in directions)
+        {
+            Vector2Int neighbourCoords = currentSearchNode.coordinates + direction;
+            if(grid.ContainsKey(neighbourCoords))
+            {
+                neighbours.Add(grid[neighbourCoords]);
+                grid[neighbourCoords].isExplored = true;
+                grid[currentSearchNode.coordinates].isPath = true;
+            }
+        }
     }
 }
